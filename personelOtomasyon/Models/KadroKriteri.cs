74b508bc7eb0cef98035cba1ad3084bdbb6f0f3b
glsmd <demirr.gulsum@gmail.com>
2025-05-05 22:51:01 +0300

    .

--- personelOtomasyon/Models/KadroKriteri.cs
+++ personelOtomasyon/Models/KadroKriteri.cs
@@ -1,3 +1,4 @@
+
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -12,11 +13,21 @@ namespace personelOtomasyon.Models
 
         [ForeignKey("IlanId")]
         public AkademikIlan Ilan { get; set; }
+
         public string KriterAdi { get; set; }
+        public string Aciklama { get; set; }
+        public bool ZorunluMu { get; set; }
+        public bool BelgeYuklenecekMi { get; set; }
+        public int BelgeSayisi { get; set; }
+
+        public string TemelAlan { get; set; }
+        public string Unvan { get; set; }
+
         public string KullaniciYoneticiId { get; set; }
 
         [ForeignKey("KullaniciYoneticiId")]
         public ApplicationUser Yonetici { get; set; }
-        public string Gereklilik { get; set; }
+
+        public ICollection<KadroKriterAlt> AltBelgeTurleri { get; set; }
     }
 }
